b7f9d3a8ce16c6dd10c6ccdb1fabd1915d5d90de
felixezequiel <felixezequiel48@gmail.com>
2025-03-16 16:45:30 -0300

    Remove middleware de controle de versão do aplicativo, simplificando a configuração do servidor. Atualiza o banco de dados de desenvolvimento.

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -1,6 +1,5 @@
 import express from 'express';
 import { setupRoutes } from '../routes';
-import { versionControl } from '../middlewares/version-control';
 import { multerErrorHandler } from '../middlewares/multer-error-handler';
 import swaggerUi from 'swagger-ui-express';
 import { swaggerSpec } from './swagger';
@@ -10,7 +9,6 @@ export function setupApp() {
 
   // Middlewares
   app.use(express.json());
-  app.use(versionControl);
 
   // Swagger
   app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
