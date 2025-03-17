a068e257c96dc9bed2a4129c469d4c6bad521636
felixezequiel <felixezequiel48@gmail.com>
2025-03-17 12:18:12 -0300

    Melhora a validação no caso de uso de importação de filmes, garantindo que os campos título, estúdio, produtor e ano sejam válidos. Adiciona verificação para evitar duplicatas na base de dados e refatora a lógica de validação para usar um loop assíncrono.

--- src/application/use-cases/movies/import-movies.ts
+++ src/application/use-cases/movies/import-movies.ts
@@ -13,42 +13,51 @@ export class ImportMoviesUseCase implements ImportMovies {
     if (!movies?.length) {
       throw new ValidationError("Nenhum filme para importar");
     }
-
-    // Validação básica dos dados
-    movies.forEach((movie, index) => {
+  
+    // Validação básica dos dados usando for await
+    for (const [index, movie] of movies.entries()) {
       if (!movie.title || movie.title?.trim() === '') {
         throw new ValidationError(
           `Filme na linha ${index + 1} não possui título`
         );
       }
+  
+      const existingMovie = await this.movieRepository.findByTitle(movie.title);
+      if (existingMovie) {
+        throw new ValidationError(
+          `Filme na linha ${index + 1} já existe na base de dados`
+        );
+      }
+  
       if (!movie.year || isNaN(movie.year) || movie.year < MOVIE_MIN_YEAR) {
         throw new ValidationError(
           `Filme na linha ${index + 1} possui ano inválido`
         );
       }
+  
       if (!movie.studios || movie.studios.trim() === '') {
         throw new ValidationError(
           `Filme na linha ${index + 1} não possui estúdio`
         );
       }
+  
       if (!movie.producers || movie.producers.trim() === '') {
         throw new ValidationError(
           `Filme na linha ${index + 1} não possui produtor`
         );
       }
-    });
-
+    }
+  
     const inserted: Movie[] = [];
-
-    // Processa em lotes para evitar sobrecarga de memória caso seja chamado diretamente em outro local que não criou o lote
+  
+    // Processa em lotes para evitar sobrecarga de memória
     for (let i = 0; i < movies.length; i += this.BATCH_SIZE) {
       const batch = movies.slice(i, i + this.BATCH_SIZE);
       const response = await this.movieRepository.bulkCreate(batch);
-
+  
       inserted.push(...response);
     }
-
+  
     return inserted;
-
   }
 }
